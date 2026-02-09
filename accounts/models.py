f507f60ac94507a8c5c5b2d542f3b25ddcf2d30b
Sebas16608 <asebasrr444@gmail.com>
2026-02-08 21:24:48 -0600

    chore: Creating the company model file

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
+    
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ("ADMIN", "Administrador"),
+        ("SELLER", "Vendedor"),
+        ("WAREHOUSE", "Almacén"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="profiles")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="SELLER")
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+    
+    def __str__(self):
+        return f"Perfil de {self.user.username} de {self.company.name} con el rol {self.role}"
