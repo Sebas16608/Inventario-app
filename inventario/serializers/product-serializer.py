bfd937892087f18f0c7828cdc1fdf6aee82e5aa5
Sebas16608 <asebasrr444@gmail.com>
2026-02-09 16:24:15 -0600

    chore: Creating the product serializer

--- inventario/serializers/product-serializer.py
+++ inventario/serializers/product-serializer.py
@@ -4,4 +4,4 @@ from inventario.models.product import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "slug", "presentation", "distribuidor", "company", "category", "created_at", "updated_at"]
+        fields = ["id", "name", "slug", "presentation", "supplier", "company", "category", "created_at", "updated_at"]
