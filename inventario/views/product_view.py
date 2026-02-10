b31f4c09a51ed37af54eb780d7985bd560363878
Sebas16608 <asebasrr444@gmail.com>
2026-02-10 15:53:27 -0600

    chore: Rename the API view for product

--- inventario/views/product_view.py
+++ inventario/views/product_view.py
@@ -2,7 +2,7 @@ from API import SuperApiView
 from inventario.models.product import Product
 from inventario.serializers import ProductSerializer
 
-class ProductView(SuperApiView):
+class ProductAPIView(SuperApiView):
     model = Product
     serializer_class = ProductSerializer
     filter_fields = ["name", "slug", "supplier"]
