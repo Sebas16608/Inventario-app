9f30c31ca6832504fa7faf8f6841cd650a7885ee
Sebas16608 <asebasrr444@gmail.com>
2026-02-12 12:52:08 -0600

    chore: Creating the init file for serializers

---
+++ accounts/serializers/__init__.py
@@ -0,0 +1,11 @@
+from .register_serializer import RegisterSerializer
+from .company_serializer import CompanySerializer
+from .user_serializer import UserSerializer
+from .profile_serializer import ProfileSerializer
+
+__all__ = [
+    "RegisterSerializer",
+    "CompanySerializer",
+    "UserSerializer",
+    "ProfileSerializer",
+]
