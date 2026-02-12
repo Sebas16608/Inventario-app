bf9fb4bd001c1cc205698c0d920dbc56670fe7df
Sebas16608 <asebasrr444@gmail.com>
2026-02-12 12:40:13 -0600

    feat: Creating the configuration for JWT

--- core/settings.py
+++ core/settings.py
@@ -75,6 +75,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# RestFramework
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
