2573fa740493178ccd97f30ab99b19b9bee187c7
Sebas16608 <asebasrr444@gmail.com>
2026-02-14 15:23:33 -0600

    fix: Fixing the init file for views2

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -1,4 +1,11 @@
-from company_view import *
-from profile_view import *
-from register_view import *
-from user_view import *
+from company_view import CompanyView
+from profile_view import ProfileView
+from register_view import RegisterView
+from login_view import loginView
+
+__all__ = [
+    "CompanyView",
+    "ProfileView",
+    "RegisterView",
+    "loginView",
+]
